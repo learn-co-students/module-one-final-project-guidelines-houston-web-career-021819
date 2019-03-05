66be8741b5e418a802edeb4b285446436c36ef1f
github username <github email address>
2019-03-05 09:22:17 -0600

    added instances to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,15 @@ Song.destroy_all
 User.destroy_all
 Playlist.destroy_all
 
-user = User.new(username: "Username", password: "Password")
+trey = User.new(username: "Trey", password: "Password")
+jing = User.new(username: "Jing", password: "Password")
 
-song = Song.new(title: "Title", artist: "Artist", album: "Album", genre: "Genre", user_id: user.id)
 
-playlist = Playlist.new(name: "Name", user_id: user.id)
+song = Song.new(title: "Title", artist: "Artist", album: "Album", genre: "Genre", user_id: trey.id)
+song2 = Song.new(title: "Title2", artist: "Artist2", album: "Album2", genre: "Genre", user_id: trey.id)
+song = Song.new(title: "Title", artist: "Artist", album: "Album", genre: "Genre", user_id: jing.id)
+song2 = Song.new(title: "Title2", artist: "Artist2", album: "Album2", genre: "Genre", user_id: jing.id)
+
+
+playlist = Playlist.new(name: "Name", user_id: trey.id)
+playlist2 = Playlist.new(name: "Name", user_id: jing.id)
