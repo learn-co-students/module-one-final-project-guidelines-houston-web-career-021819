d43a6e6d045d5cbd4eab7f6950920bd49326aedc
josieyao <yao.josephine@yahoo.com>
2019-03-05 17:25:36 -0600

    Created age method to calculate current age in user class

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
     has_many :subjects, through: :interests
+
+    def age
+        now = Time.now.utc.to_date
+        dob = self.dob
+        now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+    end
+
 end
 
