5c3e0b47c2542784e4cbfc7d2a01175e7e21a5b5
JingChen <jingchenjc2019@gmail.com>
2019-03-06 13:40:10 -0600

    CSV gem added.

--- bin/main_menu.rb
+++ bin/main_menu.rb
@@ -2,8 +2,8 @@ def main_menu(current_user)
     puts "\e[H\e[2J"
     main_menu = $prompt.select('❤What would you like to do today?❤') do |menu|
     menu.enum "."
-    menu.choice "Your music", 1
-    menu.choice "Your statistics", 2
+    menu.choice "Your Music", 1
+    menu.choice "Your Statistics", 2
     menu.choice "Suggestion", 3
     menu.choice "Interesting Facts", 4
     end
